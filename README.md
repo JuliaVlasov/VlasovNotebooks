ddc8dcb653cb1c325afdf19814e76b5d1bd7807e
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-03-14 10:29:12 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VlasovNotebooks
+
 Notebooks with some examples
+
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/juliavlasov/VlasovNotebooks/master)
---
+++ deps_install.jl
@@ -0,0 +1,14 @@
+using Pkg
+
+function install_deps()
+   io = open("REQUIRE", "r")
+   deps = read(io, String)
+   
+   for dep in split(deps)
+      Pkg.add(String(dep))
+   end
+end
+
+install_deps()
+
+using Plots, FFTW, LinearAlgebra, Statistics, BenchmarkTools
